67e4c55662476b7ee9c17e35b4963bed00b112fb
BiCKKK <bslmg08@gmail.com>
2024-10-15 17:04:11 +0100

    react libraries installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 import NetworkTopology from './components/NetworkTopology';
 // Import other components as needed
 
 const App = () => {
   return (
-    <div>
-      <NetworkTopology />
+    <div className='App'>
+      <Header/>
+      <div className='main-content'>
+        <Sidebar/>
+        <div>
+          <h1>Welcome to the Networking</h1>
+        </div>
+        <NetworkTopology />
       {/* Add other components here */}
+      </div> 
     </div>
   );
 };
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li>Home</li>
+                <li>Network Topology</li>
+                <li>Traffic Analysis</li>
+                <li>Alerts</li>
+                <li>Settings</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
