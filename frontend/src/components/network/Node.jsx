e5f8937739316da715abcd5ea399713ab91af7d7
BiCKKK <bslmg08@gmail.com>
2024-10-26 17:00:30 +0100

    Node.jsx updated

--- frontend/src/components/network/Node.jsx
+++ frontend/src/components/network/Node.jsx
@@ -1,11 +1,26 @@
-import React from "react"; 
-import { Handle } from "@xyflow/react"; 
-import ProgressBar from "../network/ProgressBar"; 
+import React from 'react'; 
+import { Handle } from '@xyflow/react'; 
+import ProgressBar from '../network/ProgressBar'; 
 
 const Node = ({ data }) => { 
   return ( 
-    <div style={{ padding: '10px', background: '#f9f9f9', borderRadius: '8px', border: '1px solid #ccc' }}> 
-      <div style={{ fontWeight: 'bold' }}>{data.label}</div> 
+    <div style={{ padding: '10px', background: '#f9f9f9', 
+      borderRadius: '8px', border: '1px solid #ccc', 
+      textAlign: 'center', minWidth: '120px', 
+      minHeight: '120px', position: 'relative' }}> 
+      {data.image && (
+        <img
+          src={data.image}
+          alt={data.label}
+          style={{
+            width: '60px',
+            height: '60px',
+            objectFit: 'cover',
+            marginBottom: '10px',
+          }}
+        />
+      )}
+      <div style={{fontWeight: 'bold'}}>{data.label}</div>
       <ProgressBar functionsInstalled={data.functionsInstalled} /> 
 
       {/* React Flow's node handles for connecting nodes */} 
